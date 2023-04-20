bcd0be33311ccd3549dd6ca83516fe945f28f065
casey.Q.Wood <caseywood252@gmail.com>
2023-04-19 18:39:00 -0700

    changed route definition and removed some commented code

--- index.js
+++ index.js
@@ -5,21 +5,17 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express()
 const port = 3000|| process.env.PORT
-// const bodyParser  = require('body-parser');
 const { response } = require('express');
 
 app.use(cors());
-// app.use(bodyParser.urlencoded());
-// app.use(bodyParser.json());
 app.use(express.json()); // Used to parse JSON bodies
 //app.use(express.urlencoded()); // Parse URL-encoded bodies using query-string library
 // or
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies using qs library
 
-
 app.get('/newSession', (req, res) => {
   console.log("new session")
-
+  
   NewInstance().then((data) => {
     return res.send({data: data});
   }).catch((err) => {
@@ -32,23 +28,18 @@ app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/', (req, res, next) => {
-  
-  console.log("Req: ", req.body)
+app.post('/generate', (req, res, next) => {
   const sessionID = req.body.id;
   const newPrompt = {role: "user", content: req.body.prompt};
   let promptContext;
 
-  console.log("sessionID: ", sessionID, " newPrompt: ", newPrompt);
   UpdateDB(sessionID, newPrompt).then((data) => {
-    console.log("Update DB return data: ", data.rows[0].message.promptContext)
-
     promptContext = data.rows[0].message.promptContext;
 
     ChatGenerate(promptContext).then((data) => {
       let newResponse = {role: "assistant", content: generatePrompt(data)};
-      UpdateDB(sessionID, newResponse).then((data) => {
-        console.log("Update DB return data: ", data.rows[0].message.promptContext.at(-1))
+
+      UpdateDB(sessionID, newResponse).then((data) => {        
         return res.send({data: data.rows[0].message.promptContext.at(-1)});
       })}).catch((err) => {
         console.log("error: ", err)
