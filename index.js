359082d6362a3652dcd7d2dcce98a2491e3f642f
casey.Q.Wood <caseywood252@gmail.com>
2023-04-19 19:42:17 -0700

    change check

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post('/generate', (req, res, next) => {
   })
 })
 
-app.listen(port,`0.0.0.0${process.env.PORT}`, () => {
+app.listen(port,`0.0.0.${process.env.PORT}`, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
